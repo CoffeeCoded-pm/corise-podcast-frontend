4e4fc9b9ef849cec062ff0f50e4963e116f056c9
Sid <sidharth.ramachandran@gmail.com>
2023-07-10 14:55:24 +0200

    Add files via upload

---
+++ podcast_frontend.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import modal
+import json
+import os
+
+def main():
+    st.title("Newsletter Dashboard")
+
+    # Left section - Input fields
+    st.sidebar.header("Podcast RSS Feed")
+
+    url = st.sidebar.text_input("RSS Feed")
+
+    process_button = st.sidebar.button("Get Podcast Information")
+
+    if process_button:
+
+        # Call the function to process the URLs and retrieve podcast guest information
+        podcast_info = process_podcast_info(url)
+
+        # Right section - Newsletter content
+        st.header("Newsletter Content")
+        
+        # Display the podcast title
+        st.subheader("Episode Title")
+        st.write(podcast_info['podcast_details']['episode_title'])
+
+        # Display the podcast summary and the cover image in a side-by-side layout
+        col1, col2 = st.columns([7, 3])
+
+        with col1:
+            # Display the podcast episode summary
+            st.subheader("Podcast Episode Summary")
+            st.write(podcast_info['podcast_summary'])
+
+        with col2:
+            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+
+        # Display the podcast guest and their details in a side-by-side layout
+        col3, col4 = st.columns([3, 7])
+
+        with col3:
+            st.subheader("Podcast Guest")
+            st.write(podcast_info['podcast_guest']['name'])
+
+        with col4:
+            st.subheader("Podcast Guest Details")
+            st.write(podcast_info["podcast_guest"]['summary'])
+
+        # Display the five key moments
+        st.subheader("Key Moments")
+        key_moments = podcast_info['podcast_highlights']
+        for moment in key_moments.split('\n'):
+            st.markdown(
+                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+
+def process_podcast_info(url):
+    f = modal.Function.lookup("corise-podcast-project", "process_podcast")
+    output = f.call(url, '/content/podcast/')
+    return output
+
+if __name__ == '__main__':
+    main()
